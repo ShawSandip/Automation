fb4572c19237be6e19a385eda98bdf06727e7060
Sandip Shaw <Surajcss14@gmail.com>
2024-04-20 15:13:37 +0530

    Test

--- src/main/java/tutorial/Testng.java
+++ src/main/java/tutorial/Testng.java
@@ -4,6 +4,9 @@ public class Testng {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is Practice automation project");
+System.out.println("This is test commit");
+
+
 	}
 
 }
